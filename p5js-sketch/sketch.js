4330932d8e7443be3156dbe239e94896c5ed14d3
Leon Xia <xjy1269058201@gmail.com>
2026-01-30 16:36:46 +0000

    Add files via upload

---
+++ p5js-sketch/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(1000, 1000);
+}
+function draw() {
+  //when mouse button is pressed, circles turn black
+  if (mouseIsPressed === true) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+
+  //white circles drawn at mouse position
+  circle(mouseX, mouseY, 50);
+}
